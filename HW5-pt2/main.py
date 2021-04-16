01828a3ccd3f632f2e2bb8eca2557d7a62b2c914
adamwainright <wainrighta16@students.ecu.edu>
2021-04-16 14:52:05 -0400

    adding db

--- HW5-pt2/main.py
+++ HW5-pt2/main.py
@@ -1,46 +1,80 @@
-import json
-import os
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+import util
+from flask_sqlalchemy import SQLAlchemy
+import os
+import csv
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+from sqlalchemy.orm import deferred
+from sqlalchemy.orm import defer, undefer
+
 
 # get current app directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = dir_path + '/data/'
 
 app = Flask(__name__)
-app.config['SQLACLCHEMY_DATABASE_URI'] = UPLOAD_FOLDER + 'NRDC_data.csv'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(dir_path, 'survey.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# list all country names with survey data
-SURVEY_DATA_COUNTRY = ['China', 'United States of America', 'United Kingdom']
+class Survey(db.Model):
+    __tablename__ = 'data-survey'
+    index = db.Column(db.Integer,  primary_key=True)
+    country =db.Column(db.Text())
+    age = db.Column(db.Integer)  
+    gender = db.Column(db.Text())
+    fearFactor = db.Column(db.Integer)
+    anxiousFactor = db.Column(db.Integer)
+    angerFactor = db.Column(db.Integer)
+    happyFactor = db.Column(db.Integer)
+    sadFactor = db.Column(db.Integer)
+    emotionFactor = db.Column(db.Text())
+    whyFactor = db.Column(db.Text())
+    meaningFactor = db.Column(db.Text())
+    job = db.Column(db.Text())
+    
+    def __init__(self, index, country, age, gender, fearFactor, anxiousFactor, angerFactor, happyFactor, sadFactor, emotionFactor, whyFactor, meaningFactor, job):
+        self.index = index
+        self.country = country
+        self.age = age
+        self.gender = gender
+        self.fearFactor = fearFactor
+        self.anxiousFactor = anxiousFactor
+        self.angerFactor = angerFactor
+        self.happyFactor = happyFactor
+        self.sadFactor = sadFactor
+        self.emotionFactor = emotionFactor
+        self.whyFactor = whyFactor
+        self.meaningFactor = meaningFactor
+        self.job = job
+        
+    def __getitem__(self, this):
+        return self.index,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job
+        
+    def __repr__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.index,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job)
+
+    def __iter__(self):
+        return iter([self.index,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job])
 
 
-@app.route('/api/query_survey_results/<country_name>', methods=['GET'])
-def query_survey_results(country_name=''):
-	if country_name in SURVEY_DATA_COUNTRY:
-		# this data should be from database
-		survey_query_data = {
-			'user_data':[
-				# index	What country do you live in?	How old are you?	What is your gender?	To what extent do you feel FEAR due to the coronavirus?	To what extent do you feel ANXIOUS due to the coronavirus?	To what extent do you feel ANGRY due to the coronavirus?	To what extent do you feel HAPPY due to the coronavirus?	To what extent do you feel SAD due to the coronavirus?	Which emotion is having the biggest impact on you?	What makes you feel that way?	What brings you the most meaning during the coronavirus outbreak?	What is your occupation?  
-				[1,"USA",70.0,"Male",2,2,1,2,2,"anticipation of whats going to happen next","Lot's of predictions from differnet resources","Family,Psychologist"],
-				[2,"Switzerland",25.0,"Female",3,4,3,4,4,"A mix of awe and anxiety. Awe at how wonderful of a challenge this is for our world to tackle together (especially on issues that have plagued us for decades) and anxiety on how much we may not be thinking about those who are most vulnerable","Reading thought leadership articles and social media","Reading,Global Public Servant (WEF)"],
-				[3,"USA",26.0,"Female",3,3,1,4,4,"A mix of happy to be safe and home and sad for people who aren’t","Seeing what’s happening in the news","Family,Student"],
-				[4,"USA",11.0,"Male",2,1,5,1,3,"Anger,No sports" ,"Family,Student"],
-				[5,"USA",28.0,"Male",4,3,4,1,4,"Anger","a system that cares about profit more than general wellbeing","trying to help","reporter"],
-				[6,"USA",24.0,"Female",4,4,5,1,4,"Anger","The US federal government" ,"Friends","Graduate student"],
-				[7,"USA",21.0,"Female",4,3,5,3,2,"Anger","People are not being responsible in doing their part in staying in doors. It is causing a major spread in our environment. Due to this spread, I am not allowed to be outdoors or stay in my apartment in Austin. I am very active outside, so this is a challenge for me to stay inside. ","Being alone,Student"],
-				[8,"USA",22.0,"Male",3,4,5,2,1,"Anger","Inaction of the people and government","Friends","Janitor"],
-				[9,"USA",39.0,"Male",2,2,4,1,2,"Anger","Impacts on day-to-day life","Exercising","Software Engineer"],
-				[10,"USA",78.0,"Male",5,5,5,1,1,"Anger","National leadership incompetence","Family","Executive - retired"]		
-			]
-		}
 
-		
-	else:
-		survey_query_data = {'user_data':[country_name+" does not have any survey data"]}
-		
-	return json.dumps(survey_query_data) 
+db.drop_all()    
+db.create_all()
+with open('data/t.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    #print(df)
+    for line in reader:
+        #print(line[0])
+        response = Survey(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9].decode('utf-8'), line[10].decode('utf-8'), line[11].decode('utf-8'), line[12].decode('utf-8'))
+        db.session.add(response)
+        db.session.commit()
+
 
 
 @app.route('/')
