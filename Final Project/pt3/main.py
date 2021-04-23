f044475bdb4e6cdd0e51d3afaf701968c6121299
younglane <59858521+younglane@users.noreply.github.com>
2021-04-23 12:57:33 -0400

    Flask not working for project
    Fixed 404 error by in putting url for tabs

--- Final Project/pt3/main.py
+++ Final Project/pt3/main.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
- 
     return render_template("home.html")
     
 @app.route("/about")
 def about():
-   
-    return render_template("about.html", )
+    return render_template("about.html")
 
 @app.route("/shows")
 def shows():
-  
     return render_template("shows.html")
 
     
